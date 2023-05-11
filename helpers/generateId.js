3a6985bbbfd491d8d15d1882f989879232b36534
Miguel Martínez <leugimmanre@gmail.com>
2023-05-11 20:25:38 +0200

    Render

--- helpers/generateId.js
+++ helpers/generateId.js
@@ -2,5 +2,5 @@ const generarId = () => {
     return Date.now().toString(32) + Math.random().toString(32).substring(2);
 };
 generarId()
-console.log(generarId())
+// console.log(generarId())
 module.exports = generarId;
--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -25,29 +25,25 @@ router.use("/doc-swagger", swaggerUi.serve);
 
 // HTTP methods
 router
-    // Init lesson 7 Outh
-    .get('/', sessionGitHub)
-    .get('/github/callback', passport.authenticate('github', {failureRedirect: '/api-docs', session: false}), passportGitHub)
-    // Init lesson 7 Outh
-    .get("/students", isAuthenticated, getStudents)
-    .get("/student/:id", isAuthenticated, GetSingleStudent)
-    .post("/new-student", isAuthenticated, validation(registerSchema), createStudent)
-    .put("/update-student/:id", isAuthenticated, validation(registerSchema), updateStudent)
-    .delete("/delete-student/:id", isAuthenticated, deleteStudent)
-    //API Documentation
-    .get("/doc-swagger", swaggerUi.setup(swaggerDocument))
-
-    // Init lesson 7 Outh
-    .get('/login', passport.authenticate('github'), (request, response) => {})
-
-    .get('/logout', function(request, response, next) {
-      // console.log(request)
-      request.session.destroy();
-        // request.logout(function(err) {
-        //   if (err) { return next(err); }
-        //   response.redirect('/');
-        // });
-        response.redirect('/')
-      });
+  .get("/students", isAuthenticated, getStudents)
+  .get("/student/:id", isAuthenticated, GetSingleStudent)
+  .post("/new-student", isAuthenticated, validation(registerSchema), createStudent)
+  .put("/update-student/:id", isAuthenticated, validation(registerSchema), updateStudent)
+  .delete("/delete-student/:id", isAuthenticated, deleteStudent)
+  //API Documentation
+  .get("/doc-swagger", swaggerUi.setup(swaggerDocument))
+  // Init lesson 7 Outh
+  .get('/', sessionGitHub)
+  .get('/github/callback', passport.authenticate('github', {failureRedirect: '/api-docs', session: false}), passportGitHub)
+  .get('/login', passport.authenticate('github'), (request, response) => {})
+  .get('/logout', function(request, response, next) {
+    // console.log(request)
+    request.session.destroy();
+      // request.logout(function(err) {
+      //   if (err) { return next(err); }
+      //   response.redirect('/');
+      // });
+      response.redirect('/')
+    });
 
 module.exports = router;
